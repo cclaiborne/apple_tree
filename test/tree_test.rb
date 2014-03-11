2a836fbcf03031831e645f67fb7c65f90f13e7cd
cclaiborne <claiborne206@gmail.com>
2014-03-10 23:21:54 -0700

    rename test title

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -2,7 +2,7 @@ require "tree"
 require "minitest/autorun"
 
 describe "a tree of employees" do
-  it "has been restructured and can be traversed using 3 depth first approaches" do
+  it "can be traversed using 3 depth first approaches" do
     tree = Tree.new('Tim', Tree.new('Johny', Tree.new('Dan'), Tree.new('Katie', Tree.new('Peter'), Tree.new('Andrea'))), Tree.new('Phil', Tree.new('Craig'), Tree.new('Eddie')))
     puts "Preorder: "
     Tree.preorder(tree)
